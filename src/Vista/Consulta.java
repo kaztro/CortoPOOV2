20876bb42eee2957fb2ff2e2f6648e516ede36ef
LN710Q <LN710Q@DEILABO1-PC30>
2018-06-27 12:41:15 -0600

    CORTO WE2

--- src/Vista/Consulta.java
+++ src/Vista/Consulta.java
@@ -8,7 +8,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class Consulta extends JFrame {
     public JLabel lblNombre, lblDirector, lblPais, lblClasificacion, lblAño, lblEn_proyeccion;
